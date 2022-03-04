4c2273bde8dfb1b5151ecd4ab31d8375cf06fca2
booennm <booennm@gmail.com>
2022-03-04 16:43:43 +0200

    fixed index to show item from shoppinglist

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'functions.php';
 
 try {
     $db = openDB();
-    $sql = "SELECT * FROM task";
+    $sql = "SELECT * FROM item";
     $query = $db->query($sql);
     $results = $query->fetchAll(PDO::FETCH_ASSOC);
     header('HTTP/1.1 200 OK');
