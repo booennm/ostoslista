c194374af94610b367af3ec10d70261534f3ad38
booennm <booennm@gmail.com>
2022-03-05 14:32:10 +0200

    delete is functional

--- delete.php
+++ delete.php
@@ -1,19 +1,19 @@
 <?php
 require_once 'headers.php';
-require_once 'funcions.php';
+require_once 'functions.php';
 
 $input = json_decode(file_get_contents('php://input'));
 $id = filter_var($input->id, FILTER_SANITIZE_SPECIAL_CHARS);
 
 try {
     $db = openDB();
-    $query = $db->prepare('DELETE FROM item WHERE id=(:id)');
+    $query = $db->prepare('delete from item where id=(:id)');
     $query->bindValue(':id', $id, PDO::PARAM_INT);
     $query->execute();
 
     header('HTTP/1.1 200 OK');
     $data = array('id' => $id);
     print json_encode($data);
-}catch (PDOException $pdoex) {
+} catch (PDOException $pdoex) {
     returnError($pdoex);
 }
